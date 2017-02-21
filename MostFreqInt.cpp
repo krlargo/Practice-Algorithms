4be2763960bbb516f9cebf58025566aabb99b5aa
Kevin Largo <xkevlar@kevins-mbp.attlocal.net>
2017-02-20 18:11:48 -0800

    MostFreqInt.cpp fully functioning. Properly finds mode but does not account for ties.

--- MostFreqInt.cpp
+++ MostFreqInt.cpp
@@ -1,6 +1,8 @@
 //"Find the most frequent integer in an array"
 
 #include<iostream>
+#include<unordered_map>
+#include<utility> //pair
 
 using namespace std;
 
@@ -30,9 +32,31 @@ void printArray(int* arr, int length) {
 }
 
 int main(int argc, char** argv) {
+	unordered_map<int, int> hashtable;
 	int length;
+	int mode, modeCount = 0;
 
 	//prompt user input	
   int* arr = getUserInput(length);
 	printArray(arr, length); 	
+
+	//for each element in array
+	for(int i = 0; i < length; i++) {
+    //if element isn't in hashtable, then add it
+		if(hashtable.find(arr[i]) == hashtable.end()) {
+			hashtable.insert(pair<int,int>(arr[i],1));
+    } else {
+			//otherwise increment its count
+			unordered_map<int, int>::iterator it = hashtable.find(arr[i]);
+			it->second += 1;
+
+			if(it->second > modeCount) {
+        mode = it->first;
+				modeCount = it->second;
+      }
+	  }
+  }
+
+	cout << "Mode: " << mode << endl;
+  cout << "Number of recurrences: " << modeCount << endl;	
 }
