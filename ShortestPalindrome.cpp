8ba3c190376bdddbe7afeb9f0cc6dea593b4dc54
krlargo <krlargo@ucdavis.edu>
2017-02-23 15:37:40 -0800

    Update ShortestPalindrome.cpp. Implemented algorithm to find largest substring in original string and reversed string.

--- ShortestPalindrome.cpp
+++ ShortestPalindrome.cpp
@@ -10,16 +10,35 @@ void getUserInput(string& input) {
 
 string reverseString(string input) {
 	string reverse = "";
-	for(int i = input.length(); i >= 0; --i) {
+	for(int i = input.length()-1; i >= 0; i--) {
    	reverse += input[i];
   }
 	return reverse;
 }
 
+//find largest substring on RHS of input
+// and LHS of reverse
 string findShortestPalindrome(string input) {
 	string reverse = reverseString(input);
-	cout << "reversed: " << reverse << endl;
-	return reverse;
+
+	int first = 0;
+	int last = input.length()-1;
+	int size = input.length(); //start at full length
+
+	while(size > first) {
+
+		cout << input.substr(last-size+1, size) << endl;
+		cout << reverse.substr(first, size) << endl << endl;
+
+		//if rhs input substr = lhs reverse substr
+		if(input.substr(last-size+1, size) == reverse.substr(first, size)) {
+			cout << "largest substr is " << reverse.substr(first, size) << endl;
+			break;
+    }
+		size--;   
+  }
+
+  return input;
 }
 
 int main(int argv, char** argc) {
