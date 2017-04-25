1c04da51e0484f37ce7349c8f33e71b487634c43
krlargo <krlargo@ucdavis.edu>
2017-04-25 00:47:19 -0700

    Implemented InsertionSort.

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -2,59 +2,35 @@
 
 using namespace std;
 
-void printArray(int* arr, int size) {
-  for(int i = 0; i < size; i++) {
-    if(i == 0)
-      cout << "{";
-    else
-      cout << ", ";
-    cout << arr[i];
-  }
-  
-  cout << "}" << endl << endl;
+void printArray(int arr[], int n) {
+	for(int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
 }
 
-void swap(int* a, int* b) {
-  int temp;
-  temp = *a;
-  *a = *b;
-  *b = temp;
+void insertionSort(int arr[], int n) {
+	for(int i = 0; i < n; i++) {
+		for(int j = i; j > 0; j--) {
+			if(arr[j] < arr[j-1])
+				swap(arr[j],arr[j-1]);
+		}
+	}
 }
 
-void insertionSort(int* arr, int size) {
-  for(int i = 0; i < size - 1; i++) {
-    for(int j = i + 1; j > 0; j--) {
-      if(arr[j] < arr[j-1]) {
-        swap(&arr[j], &arr[j-1]);
-        printArray(arr, size);
-      } else {
-        break;
-      }
-    }
-  }
-}
+int main(int argv, char** argc) {
+	int n;
+	cout << "How many elements in array?" << endl;
+	cin >> n;
 
-int main(int argv, char* argc[]) {
+	int arr[n];
+	
+	cout << "Enter " << n << " elements:" << endl;
+	for(int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
 
-  int size;
-  printf("Number of elements in array: ");
-  cin >> size;
+	printArray(arr,n);
 
-  if(size <= 0) {
-    cout << "Array is empty." << endl << endl;
-    return 0;
-  }
-
-  int arr[size];
-
-  for(int i = 0; i < size; i++)
-    cin >> arr[i];
-
-  printArray(arr, size);
-
-  insertionSort(arr, size);
-
-  printArray(arr, size);
-
-  return 0;
+	insertionSort(arr,n);
+	printArray(arr,n);
 }
