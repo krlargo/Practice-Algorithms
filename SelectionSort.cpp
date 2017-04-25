a55cc34c941caf8791aab338725fba14f4a7b433
krlargo <krlargo@ucdavis.edu>
2017-04-25 00:51:52 -0700

    Implemented SelectionSort.

---
+++ SelectionSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int arr[], int n) {
+	for(int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+void selectionSort(int arr[], int n) {
+	for(int i = 0; i < n; i++) {
+		int min = arr[i];
+		int minIndex = i;
+		for(int j = i; j < n; j++) {
+			if(arr[j] < min) {
+				min = arr[j];
+				minIndex = j;
+			}
+		}
+		swap(arr[i],arr[minIndex]);
+	}
+}
+
+int main(int argv, char** argc) {
+	int n;
+	cout << "How many elements in array?" << endl;
+	cin >> n;
+
+	int arr[n];
+	
+	cout << "Enter " << n << " elements:" << endl;
+	for(int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	printArray(arr,n);
+
+	selectionSort(arr,n);
+	printArray(arr,n);
+}
